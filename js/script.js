5e7276804f912745730531b3f836d32f1da0c0c6
Kevin Finlayson <kevin@dustandmold.net>
2012-03-26 09:20:50 -0400

    Added documentation to resizing script

--- js/script.js
+++ js/script.js
@@ -2,12 +2,15 @@
 
 function reorderHeader(){
   var viewportWidth = $(window).width();
-  if( viewportWidth <= 320 ) { /* if window width is 320 or smaller, switch the nav and profile, and add a class for styling */
-    if('header[role=banner]:not.reordered') { /* only add the ‘reordered’ class if it hasn’t been added already */
+  if( viewportWidth < 479 ) {
+    /* if window width is 320 or smaller, switch the nav and profile, and add a class for styling */
+    if('header[role=banner]:not.reordered') {
+      /* only add the ‘reordered’ class if it hasn’t been added already */
       $('header[role=banner]').addClass('reordered');
     }
     $('nav[role]=banner').insertBefore('.profile');
-  } else { /* swap the profile and nav back, and removed the ‘reordered’ class */
+  } else {
+    /* swap the profile and nav back, and removed the ‘reordered’ class */
     $('header[role=banner]').removeClass('reordered');
     $('.profile').insertBefore('nav[role]=banner');
   }
