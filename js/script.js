0f740d99079e7775fcfb0fb50941974acb74f13d
Kevin Finlayson <kevin@dustandmold.net>
2012-05-08 13:36:37 -0400

    rdio article (unfinished)

--- js/script.js
+++ js/script.js
@@ -1,28 +1,15 @@
 /* Author: Kevin Finlayson */
 
-function reorderHeader(){
-  var viewportWidth = $(window).width();
-  if( viewportWidth <= 480 ) {
-    /* if window width is 320 or smaller, switch the nav and profile, and add a class for styling */
-    if('header[role=banner]:not.reordered') {
-      /* only add the ‘reordered’ class if it hasn’t been added already */
-      $('header[role=banner]').addClass('reordered');
-    }
-    $('nav[role]=banner').insertBefore('.profile');
-  } else {
-    /* swap the profile and nav back, and removed the ‘reordered’ class */
-    $('header[role=banner]').removeClass('reordered');
-    $('.profile').insertBefore('nav[role]=banner');
-  }
-}
-
-$(window).resize(function(){
-  reorderHeader();
-})
-
-
 $(document).ready(function(){
-  reorderHeader();
+
+  $(function(){
+    $('.slideshow').slides({
+      container: 'slides-container',
+      generatePagination: false,
+      autoHeight: true
+    });
+  });
+
 });
 
 
