5d547986d4155f7400a1a4b1c42637bd6ca68741
Kevin Finlayson <kevin@kevinfinlayson.com>
2014-01-11 18:04:45 -0500

    Converted to Sassaprilla and added bower for package management

--- config.rb
+++ config.rb
@@ -1,17 +1,10 @@
-compass_config do |config|
-  config.output_style = :compact
-  config.sass_options = { :line_comments => false }
-end
+# Middleman Config
+# -----------------------------------------------------------------------------
 
-ignore '/assets/styles/*'
-ignore '/assets/scripts/*'
-
-set :css_dir, 'assets'
-set :js_dir, 'assets'
-set :images_dir, 'images'
-
+# Remove .html extension from pages
 activate :directory_indexes
 
+#  Activate middleman-blog gem
 activate :blog do |blog|
   blog.name = 'articles'
   blog.prefix = 'articles'
@@ -20,4 +13,9 @@ end
 configure :build do
   # Can you do this with Grunt?
   # activate :minify_javascript
+end
+
+compass_config do |config|
+  config.output_style = :compact
+  config.sass_options = { :line_comments => false }
 end
