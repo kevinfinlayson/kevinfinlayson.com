df99d06bf92c81a02b91b4a399db0f64d6f9fae1
Kevin Finlayson <kfinlayson@gmail.com>
2023-02-03 14:47:26 -0500

    removed console output from script.js (#10)

--- js/script.js
+++ js/script.js
@@ -14,14 +14,10 @@ $(document).ready(function(){
   // Retina image replacement
 
   var pixelRatio = !!window.devicePixelRatio ? window.devicePixelRatio : 1;
-  console.log(pixelRatio);
   if (pixelRatio > 1) {
         $('img').each(function() {
-        console.log("image");
         var retinaPath = $(this).data('retina');
-        console.log(retinaPath);
         if (retinaPath !== "") {
-          console.log("has retina path!!!");
           $(this).attr('src', retinaPath);
         }
     });
